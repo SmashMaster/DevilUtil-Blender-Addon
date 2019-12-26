22dd7981812a050d86cbf8bcb94882fb20e9dba6
Sam Johnson <x.smashmaster.x@gmail.com>
2019-12-26 10:25:03 -0700

    Deleted __init__.py

--- __init__.py
+++
@@ -1,2 +0,0 @@
-    bpy.types.Mesh.dvm_exp_mat_inds = bpy.props.BoolProperty(name = "DVM Export Material Indices", default = True)
-    bpy.types.Object.dvm_args = bpy.props.CollectionProperty(name = "DVM Arguments", type = DVMArgPropertyGroup, options = {'HIDDEN'})
